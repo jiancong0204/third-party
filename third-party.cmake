e459621a114b173f1e32fb4396d69e8b43a0b5e0
Jiancong Zheng <jiancong0204@outlook.com>
2021-10-05 23:51:40 +0800

    change dir name

--- third-party.cmake
+++ third-party.cmake
@@ -1,6 +1,6 @@
 # glog
 add_library(third_party_glog SHARED IMPORTED)
-set(GLOG_ROOT_DIR "${PROJECT_SOURCE_DIR}/third_party/glog")
+set(GLOG_ROOT_DIR "${PROJECT_SOURCE_DIR}/third-party/glog")
 set_target_properties(third_party_glog PROPERTIES
   IMPORTED_LOCATION "${GLOG_ROOT_DIR}/lib/libglog.so"
   INTERFACE_INCLUDE_DIRECTORIES "${GLOG_ROOT_DIR}/include"
@@ -8,7 +8,7 @@ set_target_properties(third_party_glog PROPERTIES
 
 # gflags
 add_library(third_party_gflags STATIC IMPORTED)
-set(GFLAGS_ROOT_DIR "${PROJECT_SOURCE_DIR}/third_party/gflags")
+set(GFLAGS_ROOT_DIR "${PROJECT_SOURCE_DIR}/third-party/gflags")
 set_target_properties(third_party_gflags PROPERTIES
   IMPORTED_LOCATION "${GFLAGS_ROOT_DIR}/lib/libgflags.a"
   INTERFACE_INCLUDE_DIRECTORIES "${GFLAGS_ROOT_DIR}/include"
@@ -16,7 +16,7 @@ set_target_properties(third_party_gflags PROPERTIES
 
 # jsoncpp
 add_library(third_party_jsoncpp SHARED IMPORTED)
-set(JSONCPP_ROOT_DIR "${PROJECT_SOURCE_DIR}/third_party/jsoncpp")
+set(JSONCPP_ROOT_DIR "${PROJECT_SOURCE_DIR}/third-party/jsoncpp")
 set_target_properties(third_party_jsoncpp PROPERTIES
   IMPORTED_LOCATION "${JSONCPP_ROOT_DIR}/lib/libjsoncpp.so"
   INTERFACE_INCLUDE_DIRECTORIES "${JSONCPP_ROOT_DIR}/include"
