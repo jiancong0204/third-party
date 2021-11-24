1000bfe56115476f6e74062ab1cb0f63983ece14
Jiancong Zheng <jiancong0204@outlook.com>
2021-11-24 23:09:43 +0800

    update cmake

--- third-party.cmake
+++ third-party.cmake
@@ -1,29 +1,11 @@
 # glog
-add_library(third_party_glog SHARED IMPORTED)
-set(GLOG_ROOT_DIR "${PROJECT_SOURCE_DIR}/third-party/glog")
-set_target_properties(third_party_glog PROPERTIES
-  IMPORTED_LOCATION "${GLOG_ROOT_DIR}/lib/libglog.so"
-  INTERFACE_INCLUDE_DIRECTORIES "${GLOG_ROOT_DIR}/include"
-)
+add_subdirectory(glog)
 
 # gflags
-add_library(third_party_gflags STATIC IMPORTED)
-set(GFLAGS_ROOT_DIR "${PROJECT_SOURCE_DIR}/third-party/gflags")
-set_target_properties(third_party_gflags PROPERTIES
-  IMPORTED_LOCATION "${GFLAGS_ROOT_DIR}/lib/libgflags.a"
-  INTERFACE_INCLUDE_DIRECTORIES "${GFLAGS_ROOT_DIR}/include"
-)
+add_subdirectory(gflags)
 
 # jsoncpp
-add_library(third_party_jsoncpp SHARED IMPORTED)
-set(JSONCPP_ROOT_DIR "${PROJECT_SOURCE_DIR}/third-party/jsoncpp")
-set_target_properties(third_party_jsoncpp PROPERTIES
-  IMPORTED_LOCATION "${JSONCPP_ROOT_DIR}/lib/libjsoncpp.so"
-  INTERFACE_INCLUDE_DIRECTORIES "${JSONCPP_ROOT_DIR}/include"
-)
+add_subdirectory(jsoncpp)
 
 # eigen3
-add_library(third_party_eigen3 INTERFACE IMPORTED)
-set_target_properties(third_party_eigen3 PROPERTIES
-  INTERFACE_INCLUDE_DIRECTORIES "${PROJECT_SOURCE_DIR}/third-party/eigen3/include/eigen3"
-)
+add_subdirectory(eigen3)
